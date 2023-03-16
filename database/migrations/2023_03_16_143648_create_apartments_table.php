00ab2495323ebc2055038bb4fcaf13aaa442dd9e
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-03-16 16:05:31 +0100

    created apartments table

---
+++ database/migrations/2023_03_16_143648_create_apartments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            //! TODO: nel seeder fare lo slug univoco con titolo + ID
+            $table->string('title', 150);
+            $table->text('description');
+            $table->text('image');
+            $table->float('latitude', 10, 8);
+            $table->float('longitude', 11, 8);
+            $table->string('address');
+            $table->tinyInteger('n_beds')->unsigned();
+            $table->tinyInteger('n_bathrooms')->unsigned();
+            $table->smallInteger('square_meters')->unsigned();
+            $table->boolean('is_visible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
