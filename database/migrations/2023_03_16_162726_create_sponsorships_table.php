06964679b4b35496b54897c525d477087f59c2d4
AngeloTripodi <tripodi.angelo@gmail.com>
2023-03-16 17:31:22 +0100

    create sponsorships table

---
+++ database/migrations/2023_03_16_162726_create_sponsorships_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sponsorships', function (Blueprint $table) {
+            $table->id();
+            $table->float('price', 3, 2);
+            $table->string('name', 10);
+            $table->tinyInteger('duration')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sponsorships');
+    }
+};
