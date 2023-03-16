97ae9cada40c547023021723f943b045282dea04
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-16 17:51:31 +0100

    Add relationship in User model and update fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Apartment;
 
 class User extends Authenticatable
 {
@@ -18,7 +19,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'date_of_birth',
         'email',
         'password',
     ];
@@ -41,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //add relationship with apartments
+    public function roles(){
+        return $this->belongsToMany(Apartment::class);
+    }
 }
