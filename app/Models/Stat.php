bbb9310a1c1fa7d18e64a57c1eaede3cb6cb3434
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-03-16 17:55:03 +0100

    added relation into stat and apartment models

--- app/Models/Stat.php
+++ app/Models/Stat.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Stat extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        //! TODO: add apartment_id column
+        'ip_address',
+        'data_view'
+    ];
+
+    public function apartment(){
+        return $this->belongsTo(Apartment::class);
+    }
 }
