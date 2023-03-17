a93432431b6d89e65070895c5e00c7b7a686c03f
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-03-17 16:38:44 +0100

    created message controller

---
+++ app/Http/Controllers/User/MessageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+}
