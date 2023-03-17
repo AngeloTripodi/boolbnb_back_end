87c8d0f2971659aea8b2419ced23a2979334473b
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-03-17 16:43:56 +0100

    added index method into message controller

--- app/Http/Controllers/User/MessageController.php
+++ app/Http/Controllers/User/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
@@ -14,7 +15,8 @@ class MessageController extends Controller
      */
     public function index()
     {
-        
+        $messages = Message::all();
+        dd($messages);
+        return view('user.message.index', compact('messages'));
     }
-
 }
