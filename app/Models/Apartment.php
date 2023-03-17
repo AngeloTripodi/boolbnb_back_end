04bc3f6e51bc9fbeaa33ef69073c9763b4b32066
francescotrudu <francescotrudu84@gmail.com>
2023-03-17 10:23:59 +0100

    Add apartment model

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -18,4 +18,13 @@ class Apartment extends Model
     {
         return $this->hasMany(Message::class);
     }
+
+    public function services()
+    {
+        return $this->belongsToMany(Service::class);
+    }
+    public function sponsorships()
+    {
+        return $this->belongsToMany(Sponsorship::class);
+    }
 }
