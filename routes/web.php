fb2e204d0a74bf520a2315168d77fcc95613ce14
MonicaDeBona <debona.monica@outlook.it>
2023-03-19 12:44:00 +0100

    add apartments route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\ApartmentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\SponsorshipController as SponsorshipController;
@@ -29,9 +30,13 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware('auth')->prefix('user')->name('user.')->group(function () {
+    Route::resource('/apartments', ApartmentController::class);
+});
+
 Route::get('/user/sponsorship', [SponsorshipController::class, 'index'])->name('user.sponsorship.index');
 
 Route::get('/user/message', [MessageController::class, 'index'])->name('user.message.index');
 Route::get('/user/message/{message}', [MessageController::class, 'show'])->name('user.message.show');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
