d602d51a642657bccd43278e8700d5f13c817aba
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-20 12:56:59 +0100

    Add * in register.blade required fields

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -64,7 +64,7 @@
 
                             <div class="mb-4 row">
                                 <label for="email"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                    class="col-md-4 col-form-label text-md-right fw-bold">{{ __('E-Mail Address') }}*</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email"
@@ -81,7 +81,7 @@
 
                             <div class="mb-4 row">
                                 <label for="password"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                    class="col-md-4 col-form-label text-md-right fw-bold">{{ __('Password') }}*</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
