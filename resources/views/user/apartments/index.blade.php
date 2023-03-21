fe9a3d2c345a09cb2a5fdf0866f2e2a07b315ed5
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-03-21 11:41:53 +0100

    added confirm to delete into index

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -30,7 +30,7 @@
                             <a href="{{ route('user.apartments.edit', $apartment->id) }}" class="btn btn-sm btn-success"><i
                                     class="fa-solid fa-pen-to-square"></i></a>
                             <form action="{{ route('user.apartments.destroy', $apartment->id) }}" method="POST"
-                                class="d-inline-block">
+                                class="d-inline-block delete double-confirm">
                                 @csrf
                                 @method('DELETE')
                                 <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash"></i></button>
@@ -40,7 +40,68 @@
                 </div>
             @endforeach
         </div>
-
-
     </div>
+<script>
+    const popup = document.getElementById('popup_message');
+    if (popup) {
+    // show a message in a toast
+        Swal.fire({
+            toast: true,
+            animation: false,
+            icon: popup.dataset.type,
+            title: popup.dataset.message,
+            type: popup.dataset.type,
+            position: 'top-right',
+            timer: 3000,
+            showConfirmButton: false,
+        });
+    }
+
+    const deleteBtns = document.querySelectorAll('form.delete');
+
+    deleteBtns.forEach((formDelete) => {
+        formDelete.addEventListener('submit', function (event) {
+        event.preventDefault();
+        var doubleconfirm = event.target.classList.contains('double-confirm');
+        Swal.fire({
+            title: 'Are you sure ?',
+            text: "Please confirm your request !",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            cancelButtonText: 'Cancel',
+            confirmButtonText: 'Yes, confirm !'
+        }).then((result) => {
+            if (result.value) {
+
+                // if double confirm
+                    if (doubleconfirm) {
+
+                        Swal.fire({
+                            title: 'Confirm request',
+                            html: "Please type <b>CONFIRM</b>",
+                            input: 'text',
+                            type: 'warning',
+                            inputPlaceholder: 'CONFIRM',
+                            showCancelButton: true,
+                            confirmButtonText: 'Confirm',
+                            cancelButtonText: 'Cancel',
+                            showLoaderOnConfirm: true,
+                        allowOutsideClick: () => !Swal.isLoading(),
+                        preConfirm: (txt) => {
+                            return (txt.toUpperCase() == "CONFIRM");
+                        },
+
+                        }).then((result) => {
+                            if (result.value) this.submit();
+                        })
+                    } else {
+                        this.submit();
+                    }
+                }
+            });
+        });
+    });
+</script>
 @endsection
