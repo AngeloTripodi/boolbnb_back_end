4a1c77c96b1a2970086e85cdeba9e95a3c1d74ad
AngeloTripodi <tripodi.angelo@gmail.com>
2023-03-21 15:35:26 +0100

    Fix name in user

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'first_name',
+        'name',
         'last_name',
         'date_of_birth',
         'email',
