c6ba4433d07228bbda955474ecc1b0bbd0a820b5
AngeloTripodi <tripodi.angelo@gmail.com>
2023-03-21 11:28:37 +0100

    fix js errors registration form

--- resources/js/client-validation.js
+++ resources/js/client-validation.js
@@ -3,18 +3,24 @@ function checkPassword() {
     let pass1 = document.getElementById("password").value;
     let pass2 = document.getElementById("password-confirm").value;
     const divElement = document.getElementById('psw-match');
-    divElement.innerHTML += `<p class="text-danger pt-1"><i class="fa-solid fa-circle-exclamation pe-1"></i> Passwords don't match </p>`
 
-    if (pass1 == pass2) {
+
+    if ((pass1.length > 7) && (pass1 == pass2)) {
 
         return true;
-    } else {
+    } else if (pass1.length < 7) {
+        console.log("Password must be at least 8 characters");
+        divElement.innerHTML += `<p class="text-danger pt-1"><i class="fa-solid fa-circle-exclamation pe-1"></i> Password must be at least 8 characters </p>`;
+        return false;
+    }
+    else if (pass1 != pass2) {
         console.log("Passwords do not match");
+        divElement.innerHTML += `<p class="text-danger pt-1"><i class="fa-solid fa-circle-exclamation pe-1"></i> Passwords don't match </p>`
         return false;
     }
 }
 
-document.querySelector('form').addEventListener('submit', function(e) {
+document.querySelector('form').addEventListener('submit', function (e) {
     //Prevent default behaviour
     e.preventDefault();
     //Check passwords
