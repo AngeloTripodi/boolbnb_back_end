8fc71583341a33141e5fb29577ab2b339a7d9338
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-03-24 14:50:45 +0100

    fixed show buttons

--- resources/views/user/apartments/show.blade.php
+++ resources/views/user/apartments/show.blade.php
@@ -11,8 +11,9 @@
                 <div class="col-6 d-flex justify-content-end">
                     <div class="actions pe-3">
 
-                        <a href="{{ route('user.apartments.index') }}" class="btn">    
+                        <a href="{{ route('user.apartments.index') }}" class="btn card_btn">    
                             <i class="fa-solid fa-arrow-left"></i>
+                            Go back to the apartments
                         </a>
                         <form action="{{ route('user.toggle', $apartment->id) }}" method="POST" class="d-inline">
                             @method('PATCH')
@@ -21,15 +22,19 @@
                                 <i class="fa-regular {{ $apartment->is_visible ? 'fa-eye visibility-icon' : 'fa-eye-slash visibility-icon' }}"></i>
                             </button>
                         </form>
-                        <a href="{{ route('user.apartments.edit', $apartment->id) }}" class="btn">
+                        <a href="{{ route('user.apartments.edit', $apartment->id) }}" class="btn card_btn">
                             <i class="fa-solid fa-edit"></i>
+                            Edit
                         </a>
                         <form class="d-inline-block form-delete double-confirm delete"
                             action="{{ route('user.apartments.destroy', $apartment->id) }}" method="POST"
                             data-element-name="{{ $apartment->title }}">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" title="Delete" class="btn"><i class="fa-solid fa-trash"></i></button>
+                            <button type="submit" title="Delete" class="btn card_btn">
+                                <i class="fa-solid fa-trash"></i>
+                                Delete
+                            </button>
                         </form>
 
                     </div>
