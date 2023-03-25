c3d753eecd57bae2767f17c239b0831b9bbe21ee
francescotrudu <francescotrudu84@gmail.com>
2023-03-25 15:39:37 +0100

    Added services filter

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -9,14 +9,32 @@ use Illuminate\Http\Request;
 class ApartmentController extends Controller
 {
     //
-    public function index()
+    public function index(Request $request)
     {
-        $apartments = Apartment::with('services')->get();
+        //Join services table
+        $apartments = Apartment::with('services')
+            // condition when(boolean, callback function) to filter services only if requested
+            ->when($request->input('services'), function ($query, $services) {
+                // foreach service build the query with whereHas method to define additional query constraints
+                foreach ($services as $service) {
+                    $query->whereHas('services', function ($query) use ($service) {
+                        // TODO use slug for now
+                        $query->where('slug', $service);
+                    });
+                }
+            })
+            // TODO orderBy title for now
+            ->orderBy('title', 'asc')
+            ->get();
+
         return response()->json([
             'success' => true,
             'results' => $apartments
         ]);
     }
+
+
+
 
     //find or fail da fare 404 con Header? Forse...
     public function show(Apartment $apartment)
