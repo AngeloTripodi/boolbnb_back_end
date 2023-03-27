1035adeec3d259e1d182d8e038c3d90fe5ac537d
francescotrudu <francescotrudu84@gmail.com>
2023-03-27 10:22:41 +0200

    style login register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,15 +11,16 @@
 </head>
 <body>
     <section id="login">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col col-xl-10">
-                    <div class="card my-shadow" style="border-radius: 1rem">
+        <div class="container py-5">
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col col-xl-8">
+                    <div class="card my-shadow b-radius-1">
                         <div class="row g-0">
                             <div class="col-md-6 col-lg-5 d-none d-md-block">
                                 <img src="{{ asset('img/' . 'bg-house.jpg') }}"
-                                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem" />
+                                alt="login form" class="img-fluid b-radius-left-1" />
                             </div>
+                            
                             <div class="col-md-6 col-lg-7 d-flex align-items-center">
                                 <div class="card-body p-4 p-lg-5 text-black">
                                     
@@ -28,11 +29,11 @@
                                         {{-- Logo --}}
                                         <div class="d-flex align-items-center mb-3 pb-1">
                                             {{-- insert logo here --}}
-                                            <i class="fa-solid fa-b fa-2x me-3" style="color: #fe8376;"></i>
+                                            <i class="fa-solid fa-b fa-2x me-3 main-color"></i>
                                             <span class="h1 fw-bold mb-0">Boolbnb</span>
                                         </div>
                                         
-                                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+                                        <h5 class="fw-normal mb-3 pb-3">Sign into your account</h5>
                                         
                                         {{-- Email --}}
                                         <div class="form-outline mb-4">
@@ -67,7 +68,7 @@
                                             </label>
                                         </div>
                                         <div class="pt-1 mb-4 w-100">
-                                            <button class="btn btn-lg w-100 text-white" type="submit" style="background-color: #fe8376">{{ __('Login') }}</button>
+                                            <button class="btn btn-lg w-100 my-btn" type="submit">{{ __('Login') }}</button>
                                         </div>
                                         
                                         {{-- forgot password --}}
@@ -75,7 +76,7 @@
                                         <a class="small text-muted text-decoration-none" href="{{ route('password.request') }}">{{ __('Forgot Password?') }}</a>
                                         @endif
                                         <div class="register">
-                                            <span class="pb-lg-2">Don't have an account? <a href="{{ route('register') }}" class="text-decoration-none" style="color: #fe8376;">{{ __('Register here') }}</a></span>
+                                            <span class="pb-lg-2">Don't have an account? <a href="{{ route('register') }}" class="text-decoration-none main-color">{{ __('Register here') }}</a></span>
                                         </div>
                                     </form>
                                 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,29 +12,29 @@
 
 <body>
     <section id="register">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col col-xl-10">
-                    <div class="card my-shadow" style="border-radius: 1rem">
+        <div class="container py-5">
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col col-xl-8">
+                    <div class="card my-shadow b-radius-1 bg-main-color border-0">
                         <div class="row g-0 align-items-end">
-
+                            
                                 <div class="col-md-6 col-lg-5 d-none d-md-block">
                                         <img src="{{ asset('img/' . 'bg-house.jpg') }}"
-                                        alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem">
+                                        alt="login form" class="img-fluid b-radius-left-1">
                                 </div>
-                                <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div class="col-md-6 col-lg-7 d-flex align-items-center bg-white border b-radius-right-1 b-radius-m">
                                 <div class="card-body p-4 p-lg-5 text-black">
                                     <form class="form needs-validation" novalidate method="POST" action="{{ route('register') }}">
                                         @csrf
                                         {{-- Logo --}}
                                         <div class="d-flex align-items-center mb-3 pb-1">
                                             {{-- insert logo here --}}
-                                            <i class="fa-solid fa-b fa-2x me-3" style="color: #fe8376;"></i>
+                                            <i class="fa-solid fa-b fa-2x me-3 main-color"></i>
                                             <span class="h1 fw-bold mb-0">Boolbnb</span>
                                         </div>
             
                                         {{-- Name --}}
-                                        <div class="form-outline mb-4">
+                                        <div class="form-outline mb-3">
                                             <label class="form-label m-1" for="name">{{ __('Name') }}</label>
                                             <input id="name" type="text" class="form-control form-control-lg @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus />
                                             <div class="text-danger" id="name-error-message"></div>
@@ -46,7 +46,7 @@
                                         </div>
             
                                         {{-- Last name --}}
-                                        <div class="form-outline mb-4">
+                                        <div class="form-outline mb-3">
                                             <label class="form-label m-1" for="last_name">{{ __('Last name') }}</label>
                                             <input id="last_name" type="text" class="form-control form-control-lg @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus />
                                             <div class="text-danger" id="last-name-error-message"></div>
@@ -58,7 +58,7 @@
                                         </div>
             
                                         {{-- Date of Birth --}}
-                                        <div class="form-outline mb-4">
+                                        <div class="form-outline mb-3">
                                             <label class="form-label m-1" for="date_of_birth">{{ __('Date of birth') }}</label>
                                             <input id="date_of_birth" type="date" class="form-control form-control-lg @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth" autofocus />
                                             <div class="text-danger" id="dob-error-message"></div>
@@ -70,8 +70,8 @@
                                         </div>
 
                                         {{-- Email --}}
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label m-1" for="email">{{ __('E-Mail Address')}}</label>
+                                        <div class="form-outline mb-3">
+                                            <label class="form-label m-1" for="email">{{ __('E-Mail Address')}}*</label>
                                             <input id="email" type="email" class="form-control form-control-lg @error('email') is-invalid @enderror"
                                             name="email" value="{{ old('email') }}" required autocomplete="email" autofocus />
                                             <div class="text-danger" id="email-error-message"></div>
@@ -83,8 +83,8 @@
                                         </div>
             
                                         {{-- Password --}}
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label m-1" for="password">{{ __('Password') }}</label>
+                                        <div class="form-outline mb-3">
+                                            <label class="form-label m-1" for="password">{{ __('Password') }}*</label>
                                             <input id="password" type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="password" autofocus />
                                             <div class="text-danger" id="password-error-message"></div>
                                             @error('password')
@@ -95,8 +95,8 @@
                                         </div>
             
                                         {{-- Password confirm --}}
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label m-1" for="password-confirm">{{ __('Confirm Password') }}</label>
+                                        <div class="form-outline mb-3">
+                                            <label class="form-label m-1" for="password-confirm">{{ __('Confirm Password*') }}</label>
                                             <input id="password-confirm" type="password" class="form-control form-control-lg" name="password_confirmation" required autocomplete="new-password" autofocus />
                                             <div class="text-danger" id="confirm-password-error-message"></div>
                                             @error('password')
@@ -106,9 +106,9 @@
                                             @enderror
                                         </div>
             
-                                        <div class="mb-4 row mb-0">
-                                            <div class="pt-1 mb-4 w-100">
-                                                <button type="submit" class="btn btn-lg w-100 text-white" style="background-color: #fe8376">
+                                        <div class="mb-3 row mb-0">
+                                            <div class="pt-1 mb-3 w-100">
+                                                <button type="submit" class="btn btn-lg w-100 my-btn">
                                                     {{ __('Register') }}
                                                 </button>
                                             </div>
