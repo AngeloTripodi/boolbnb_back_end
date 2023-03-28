09a51a2386c0aae8ba217cbf4d0f0c0f1d960f93
MonicaDeBona <debona.monica@outlook.it>
2023-03-28 15:37:48 +0200

    add whereIn for services

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -15,13 +15,11 @@ class ApartmentController extends Controller
         $apartments = Apartment::with('services')
             // condition when(boolean, callback function) to filter services only if requested
             ->when($request->input('services'), function ($query, $services) {
-                // foreach service build the query with whereHas method to define additional query constraints
-                foreach ($services as $service) {
-                    $query->whereHas('services', function ($query) use ($service) {
-                        // TODO use slug for now
-                        $query->where('slug', $service);
-                    });
-                }
+                $query->whereHas('services', function ($query) use ($services) {
+                    // whereIn per ottenere gli appartamenti che hanno tutti i servizi richiesti
+                    $query->whereIn('slug', $services);
+                    //count ci indica il numero di servizi nella query, quindi filtriamo gli appartamenti che hanno un numero di servizi corrispondente a quello della query
+                }, '=', count($services));
             })
             ->when($request->has('address'), function ($query) use ($request) {
                 $query->where('address', 'like', '%' . $request->input('address') . '%');
@@ -72,9 +70,6 @@ class ApartmentController extends Controller
     //     'success' => true,
     //     'results' => $apartments
     // ]);
-
-
-
 
 
     //find or fail da fare 404 con Header? Forse...
