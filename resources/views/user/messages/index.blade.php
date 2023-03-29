461c2b15aacd5349e3247e9c12e5e042524563b8
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-29 18:51:09 +0200

    Add mail to button in message index

--- resources/views/user/messages/index.blade.php
+++ resources/views/user/messages/index.blade.php
@@ -11,7 +11,11 @@
                    <h2>{{$message->email}}</h2>
                    <h5>{{$message->phone_number}}</h5>
                    <p>{{$message->message}}</p>
+                   <a class="btn btn-primary" :href="mailto:{{$message->email}}">
+                        Reply 
+                   </a>
                 @endforeach
+
 
             </div>
         </div>
