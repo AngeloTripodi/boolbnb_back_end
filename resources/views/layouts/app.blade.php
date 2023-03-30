ee3d282dd0d5d69c1f3b063b002a72872f7f2162
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-30 10:58:26 +0200

    Add Messages link in app.blade logged navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,6 +80,8 @@
                                         <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
                                         <a class="dropdown-item"
                                             href="{{ route('user.apartments.index') }}">{{ __('Apartments') }}</a>
+                                            <a class="dropdown-item"
+                                            href="{{ route('user.messages.index') }}">{{ __('Messages') }}</a>
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                         document.getElementById('logout-form').submit();">
