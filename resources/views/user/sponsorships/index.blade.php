a5734fa238e40a4938dded238d949eaa70a0ae4e
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-31 17:22:55 +0200

    Add hover color in sidebar

--- resources/views/user/sponsorships/index.blade.php
+++ resources/views/user/sponsorships/index.blade.php
@@ -4,27 +4,23 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <div>
-                        <div class="py-4">
-                            <h1 class="text-uppercase">
+                    <div class="py-4">
+                        <h1 class="text-uppercase">
                                 Your Sponsorships
-                            </h1>
-                            <h6 class="fs-5 pb-3">
+                        </h1>
+                        <h6 class="fs-5 pb-3">
                                 Sponsor immediatly your apartments.
-                            </h6>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 d-flex justify-content-between">
-                                @foreach ($sponsorships as $sponsorship)
-                                <div class="card p-5 col-12-sm">
-                                    <h5>Sponsorship level: <span class="fw-bold" >{{ucfirst($sponsorship->name)}}</span> </h5> 
-                                    <p>Price: {{$sponsorship->price}} $</p>
-                                    <a class="btn my-btn fw-bold py-2" href="{{ route('user.sponsorships.checkout', ['apartment' => $apartment->id, 'sponsorship' => $sponsorship->id]) }}">Select</a>
-                                </div>
-                                @endforeach
+                        </h6>
+                    </div>
+                    <div class="row">
+                        @foreach ($sponsorships as $sponsorship)
+                            <div class="col-12 col-sm-12 col-lg-4 p-3 border rounded-3 mb-4">
+                                <h5>Sponsorship level: <span class="fw-bold" >{{ucfirst($sponsorship->name)}}</span> </h5> 
+                                <p>Price: {{$sponsorship->price}} $</p>
+                                <a class="btn my-btn fw-bold py-2" href="{{ route('user.sponsorships.checkout', ['apartment' => $apartment->id, 'sponsorship' => $sponsorship->id]) }}">Select</a>
                             </div>
-                        </div>
-                </div>
+                        @endforeach
+                    </div>
             </div>
             </div>
         </div>
