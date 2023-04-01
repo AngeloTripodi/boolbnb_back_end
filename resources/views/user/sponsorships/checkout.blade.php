75ab74db8b727261770b4d413c4ed4d6f1b7413c
MonicaDeBona <debona.monica@outlook.it>
2023-04-02 00:42:51 +0200

    Add recap message on sponsorship checkout

--- resources/views/user/sponsorships/checkout.blade.php
+++ resources/views/user/sponsorships/checkout.blade.php
@@ -12,15 +12,22 @@
                         Pay in security your best sponsorship.
                     </h6>
                 </div>
-
-                <form id="payment-form" action="{{ route('user.sponsorships.store', ['apartment' => $apartment->id, 'sponsorship' => $sponsorship->id]) }}" method="POST">
+                <h5 class="sponsorship-recap-message">
+                    You are buying the <span class="fw-bold">"{{ $sponsorship->name }} sponsorship"</span> for the apartment
+                    <span class="fw-bold">"{{ $apartment->title }}"</span>
+                    with total price: <span class="fw-bold" id="sponsorship-price">&euro;
+                        {{ $sponsorship->price }}</span>
+                </h5>
+                <form id="payment-form"
+                    action="{{ route('user.sponsorships.store', ['apartment' => $apartment->id, 'sponsorship' => $sponsorship->id]) }}"
+                    method="POST">
                     @csrf
                     <!-- Putting the empty container you plan to pass to
-                      `braintree.dropin.create` inside a form will make layout and flow
-                      easier to manage -->
+                                             `braintree.dropin.create` inside a form will make layout and flow
+                                               easier to manage -->
                     <div id="dropin-container"></div>
                     <input class="btn my-btn text-right" type="submit" />
-                    <input class="btn my-btn text-right" type="hidden" id="nonce" name="payment_method_nonce"/>
+                    <input class="btn my-btn text-right" type="hidden" id="nonce" name="payment_method_nonce" />
                 </form>
             </div>
         </div>
@@ -30,4 +37,3 @@
 @section('script')
     @vite('resources/js/sponsorship.js')
 @endsection
-
