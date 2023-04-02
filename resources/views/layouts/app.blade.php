5918fc77f8a03f4ece1bd354e99916f7996491e2
francescotrudu <francescotrudu84@gmail.com>
2023-04-02 12:06:54 +0200

    Fix color and font-size

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,7 +79,7 @@
                                         {{ Auth::user()->name ?? Auth::user()->email }}
                                     </a>
 
-                                    <div class="dropdown-menu my_dropdown-menu dropdown-menu-right fs-5"
+                                    <div class="dropdown-menu my_dropdown-menu dropdown-menu-right"
                                         aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
                                         <a class="dropdown-item"
